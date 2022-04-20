7b5daf82b18f3ecb03594d7866ffa1299a0fe9dd
Francisco Rosete <fjrosete@gmail.com>
2022-04-20 14:09:40 -0700

    added game reset function

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(typeof guess);
 
     // GUESS CHECK FUNCTION //////////////////////////////////////////////
+    // No input
     if(!guess) {
         document.querySelector('.message').textContent = 'No Number';
+    //Correct guess
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = 'Correct Number';
+        document.querySelector('.number').textContent = secretNumber;
+        
+        document.querySelector('body').style.backgroundColor ='#60b347';
+
+        document.querySelector('.number').style.width = '30rem';
+    //High guess
     } else if (guess > secretNumber) {
         if (score > 1) {
             document.querySelector('.message').textContent = 'Too High!';
@@ -23,7 +29,7 @@ document.querySelector('.check').addEventListener('click', function() {
             document.querySelector('.message').textContent = 'You lost the game';
             document.querySelector('.score').textContent = 0;
         }
-        
+    // Low guess
     } else if (guess < secretNumber) {
         if (score > 1) {
             document.querySelector('.message').textContent = 'Too Low!';
@@ -34,4 +40,19 @@ document.querySelector('.check').addEventListener('click', function() {
             document.querySelector('.score').textContent = 0;
         }
     }
+});
+
+// GAME RESET FUNCTION /////////////////////////////////////////////////////
+document.querySelector('.again').addEventListener('click', function() {
+    // RESET LOGIC ---------------------------------
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.message').textContent = 'Start Guessing...';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+
+    // RESET STYLES ---------------------------------------------
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
 });
